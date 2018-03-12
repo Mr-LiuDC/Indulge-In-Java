03f4c5a78654341a7d55e98296ff87b150b96b61
Mr-LiuDC <442823529@qq.com>
2018-03-13 07:49:31 +0800

    add project springboot-security-thymeleaf-registration-login

---
+++ Springboot-In-Practice/springboot-security-thymeleaf-registration-login/src/main/java/springboot/SpringbootSecurityThymeleafRegistrationLoginApplication.java
@@ -0,0 +1,12 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityThymeleafRegistrationLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityThymeleafRegistrationLoginApplication.class, args);
+	}
+}
