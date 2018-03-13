414163c5140a8bc7294306ced2047f30ebdfa722
Mr-LiuDC <442823529@qq.com>
2018-03-13 21:41:24 +0800

    fix ArticleRepositroy bug

--- Springboot-In-Practice/springboot-multi-table/src/main/java/springboot/repositroy/ArticleRepositroy.java
+++ Springboot-In-Practice/springboot-multi-table/src/main/java/springboot/repositroy/ArticleRepositroy.java
@@ -1,13 +1,13 @@
 package springboot.repositroy;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import springboot.domain.Article;
 
 public interface ArticleRepositroy extends JpaRepository<Article, Long> {
 
-	Page<Article> findArticlesByTitleContainsOrContentContains(String keyWord, PageRequest pageable);
+	Page<Article> findArticlesByTitleContainsOrContentContains(String keyWord, Pageable pageable);
 
 }
