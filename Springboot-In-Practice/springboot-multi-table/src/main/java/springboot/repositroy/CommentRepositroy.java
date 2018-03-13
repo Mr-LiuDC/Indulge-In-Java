acaa675f0ab2f9f6cba491e2397baef66177217a
Mr-LiuDC <442823529@qq.com>
2018-03-13 21:41:00 +0800

    fix CommentRepositroy bug

--- Springboot-In-Practice/springboot-multi-table/src/main/java/springboot/repositroy/CommentRepositroy.java
+++ Springboot-In-Practice/springboot-multi-table/src/main/java/springboot/repositroy/CommentRepositroy.java
@@ -1,7 +1,6 @@
 package springboot.repositroy;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,7 +8,7 @@ import springboot.domain.Comment;
 
 public interface CommentRepositroy extends JpaRepository<Comment, Long> {
 
-	Page<Comment> findCommentsByContentContains(String content, PageRequest pageable);
+	Page<Comment> findCommentsByContentContains(String content, Pageable pageable);
 
 	Page<Comment> findCommentsByArticle_Id(Long articleId, Pageable pageable);
 
