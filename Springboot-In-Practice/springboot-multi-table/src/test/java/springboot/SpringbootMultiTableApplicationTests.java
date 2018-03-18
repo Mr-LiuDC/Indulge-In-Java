e8f136156783bc96c36cc1a4f6616748080f811e
liudecai <liudecai@postop.cn>
2018-03-18 11:37:12 +0800

    add test class

--- Springboot-In-Practice/springboot-multi-table/src/test/java/springboot/SpringbootMultiTableApplicationTests.java
+++ Springboot-In-Practice/springboot-multi-table/src/test/java/springboot/SpringbootMultiTableApplicationTests.java
@@ -1,16 +1,37 @@
 package springboot;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootMultiTableApplicationTests {
 
+	@Autowired
+	private WebApplicationContext context;
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.webAppContextSetup(context).build(); // 构造MockMvc
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testLazyLoad() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/api/comment").accept(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.page.content.length()").value(3))
+				.andExpect(jsonPath("$.page.content[1].content").value("四海为家"));
 	}
 
 }
