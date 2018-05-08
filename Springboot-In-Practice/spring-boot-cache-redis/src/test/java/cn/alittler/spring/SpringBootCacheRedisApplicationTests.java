d5e328a5e28185dc4ecbb4e1dca4b7d55f357826
liudecai <liudecai@postop.cn>
2018-05-08 17:38:47 +0800

    [Add]添加spring-boot-cache-redis

---
+++ Springboot-In-Practice/spring-boot-cache-redis/src/test/java/cn/alittler/spring/SpringBootCacheRedisApplicationTests.java
@@ -0,0 +1,20 @@
+package cn.alittler.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author LiuDeCai
+ * @date 2018/05/08
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCacheRedisApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
