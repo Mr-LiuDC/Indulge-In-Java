b988d620b8304b3c132f9a7f3f5bb17c79225216
liudecai <liudecai@postop.cn>
2018-05-08 16:24:32 +0800

    [Add]添加spring-boot-memcached项目

---
+++ Springboot-In-Practice/spring-boot-memcached/src/main/java/cn/alittler/spring/SpringBootMemcachedApplication.java
@@ -0,0 +1,16 @@
+package cn.alittler.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LiuDeCai
+ * @date 2018/05/08
+ */
+@SpringBootApplication
+public class SpringBootMemcachedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMemcachedApplication.class, args);
+    }
+}
